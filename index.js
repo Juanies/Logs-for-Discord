18cf85c9031ceefb6c5515b8aa8a9484151daf16
Juan Romero <80675013+Juanies@users.noreply.github.com>
2023-03-26 15:34:30 +0200

    time in the logs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const { Client, GatewayIntentBits, MessageCollector } = require('discord.js'); // npm package discord.js
 const { token } = require('./config.json'); // JSON with the token
 const fs = require('fs'); // npm package fs
@@ -12,14 +11,23 @@ const client = new Client({
 client.on('messageCreate', async interaction => {
 
     // We can see the available values of each message
-    console.log(interaction)
 
     const username = interaction.author.username    // We save the author name of each message ( Lucas )
     const usernameID = interaction.author.discriminator // We save the author ID of each message ( #0000 )
     const message = interaction.content // We save the message of each message ( Hello Wold! )
 
+    const date = new Date(); 
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
     fs.appendFile('.\archivos\LogsChat.txt', // Directory where is located the file with the log messages 
-    `[ ${username} #${usernameID} ]  ${message}`  + `\n`, function (err) { // Message of each logged message
+    `[ ${username} #${usernameID} ] : [ ${hours}:${minutes}:${seconds}   / ${day}/${month}/${year}  ]  ${message}`  + `\n`, function (err) { // Message of each logged message
         if (err) throw err;
         console.log('Updated!'); // Menssage that indicates that the log/message has been registered correctly
     });
@@ -27,3 +35,7 @@ client.on('messageCreate', async interaction => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+
+
+
